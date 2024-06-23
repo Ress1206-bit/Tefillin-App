5f3b7f66ad79e0beefea9c331b0b10bc515bc11d
Ress1206-bit <adam.ress@icloud.com>
2024-06-23 11:57:53 -0400

    Initial Commit

---
+++ Tefillin-App/Tefillin_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tefillin_AppApp.swift
+//  Tefillin-App
+//
+//  Created by Adam Ress on 6/23/24.
+//
+
+import SwiftUI
+
+@main
+struct Tefillin_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
